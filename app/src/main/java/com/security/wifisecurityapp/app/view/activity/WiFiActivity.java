8bc17d06c5b94b5842b686cc66bec6a9327ede87
zeinaoliveira <ze220988>
2015-08-26 14:58:13 -0400

    Add wifi manager

--- app/src/main/java/com/security/wifisecurityapp/app/view/activity/WiFiActivity.java
+++ app/src/main/java/com/security/wifisecurityapp/app/view/activity/WiFiActivity.java
@@ -1,32 +1,97 @@
 package com.security.wifisecurityapp.app.view.activity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.security.wifisecurityapp.R;
+import com.security.wifisecurityapp.app.bo.WiFiInfoBO;
+import com.security.wifisecurityapp.app.model.InfoWiFi;
+import com.security.wifisecurityapp.app.view.adapter.WiFiRouterAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WiFiActivity extends AppCompatActivity {
 
-    private TextView txtEmptyList;
 
-    private ListView lstWifiRouter;
+    TextView txtEmptyList;
+    TextView editWiFi;
+    ListView lstWifiRouter;
+    InfoWiFi wifiInfo;
+    List<InfoWiFi> wifiList;
+    WifiManager wifiManager;
+    WiFiInfoBO wifiInfoBO;
+    WiFiRouterAdapter adapter;
+    AlertDialog informationDialog;
+    ProgressDialog progressBar;
 
-    private void initialize(){
+    /**
+     * Initialize objects.
+     */
+    private void initialize() {
+
         txtEmptyList = (TextView) findViewById(R.id.txtEmptyList);
+        editWiFi = (TextView) findViewById(R.id.txtEmptyList);
         lstWifiRouter = (ListView) findViewById(R.id.list_wifiRouter);
+        wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+
+        wifiInfoBO = new WiFiInfoBO();
+        wifiList = new ArrayList<InfoWiFi>();
+
+        informationDialog = new AlertDialog.Builder(this).create();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wi_fi);
+
         initialize();
+        createListRouters();
+
+        lstWifiRouter.setOnItemClickListener(new  AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent,
+                                    View view, int position, long id) {
+                InfoWiFi infoWifi = (InfoWiFi) parent.getAdapter().getItem(position);
+                setWiFiInformationDialog(infoWifi);
+            }
+        });
     }
 
+    /**
+     * Show informations about network in a dialog.
+     */
+    public void setWiFiInformationDialog(InfoWiFi infoWifi) {
+        String title;
+
+        informationDialog.setTitle(R.string.network_information);
+
+        if (infoWifi.getSsid().isEmpty())
+            title = "• " + getString(R.string.ssid)+ ": " + getString (R.string.hiddenSSID) + "\n\n";
+        else
+            title = "• " + getString(R.string.ssid)+ ": " + infoWifi.getSsid() + "\n\n";
+
+        title += 	"• " + getString(R.string.frequency) + ": " + infoWifi.getFrequency() + " MHz\n\n" +
+                "• " + getString(R.string.capabilities) + ": "  + infoWifi.getCapabilities() + "\n\n" +
+                "• " + getString(R.string.security_level) + ": " + infoWifi.getSecurityofRouter() + "%\n";
+
+        informationDialog.setMessage(title);
+        informationDialog.show();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -54,5 +119,51 @@ public class WiFiActivity extends AppCompatActivity {
     public void showAbout() {
         Intent intent = new Intent (WiFiActivity.this, AboutActivity.class);
         startActivity(intent);
+    }
+
+    public void refresh() {
+
+
+        progressBar = ProgressDialog.show(this, getString(R.string.refresh), getString(R.string.refreshing), false,false);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try
+                {
+                    Thread.sleep(3000);
+                } catch(Exception e){}
+                progressBar.dismiss();
+            }
+        }).start();
+        refreshList();
+        txtEmptyList.setText("");
+
+    }
+
+    public void refreshList() {
+        wifiList.clear();
+        createListRouters();
+        Toast.makeText(WiFiActivity.this, R.string.refresh_list, Toast.LENGTH_SHORT).show();
+    }
+
+    public void createListRouters() {
+
+        if (wifiManager.getScanResults().size() > 0) {
+
+            for (int i = 0; i < wifiManager.getScanResults().size(); i++) {
+                wifiInfo = new InfoWiFi();
+                wifiInfo.setCapabilities(wifiManager.getScanResults().get(i).capabilities);
+                wifiInfo.setSsid(wifiManager.getScanResults().get(i).SSID);
+                wifiInfo.setFrequency(wifiManager.getScanResults().get(i).frequency);
+//	    		wifiInfo.setTimestamp(wifiManager.getScanResults().get(i).timestamp);
+                wifiInfo.setSecurityofRouter(wifiInfoBO.getSecurityofRouter(wifiInfo));
+                wifiList.add(wifiInfo);
+
+                adapter = new WiFiRouterAdapter(this, wifiList);
+                lstWifiRouter.setAdapter(adapter);
+            }
+        } else
+            txtEmptyList.setText(R.string.emptyList);
     }
 }
